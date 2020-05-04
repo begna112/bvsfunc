10bd55d29389e0ec2ae4df7c0416bad73c5da205
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-05-04 09:54:10 +0200

    Further update arguments, description, imports;  minor changes to function
    Made it overall more readable and clear what accepts what.
    
    I've also made the default descaling kernels more sane. 1/3, 1/3 will always cause a shift, even if the dimensions aren't adjusted. 0, 1/2 is also a more common default anyway.
    
    nnedi3_resample import has been slightly adjusted for readability.

--- src/bvsfunc/mods/descaleaamod.py
+++ src/bvsfunc/mods/descaleaamod.py
@@ -1,6 +1,7 @@
 import re
+from fractions import Fraction
 from functools import partial
-from typing import Optional
+from typing import Callable, Optional, Union
 
 from vsutil import get_w
 
@@ -10,9 +11,13 @@ core = vs.core
 
 def DescaleAAMod(src: vs.VideoNode,
                  w: Optional[int] = None, h: int = 720, thr: int = 10,
-                 kernel: str ='bicubic', b: float = 0, c: float = 1/2, taps: int = 3,
+                 kernel: str ='bicubic',
+                 b: Union[float, Fraction] = Fraction(0),
+                 c: Union[float, Fraction] = Fraction(1, 2),
+                 taps: int = 4,
                  expand: int = 3, inflate: int = 3,
-                 showmask: bool = False) -> vs.VideoNode:
+                 showmask: bool = False,
+                 **upscale_args) -> vs.VideoNode:
     """
     Mod of DescaleAA to use nnedi3_resample, which produces sharper results than nnedi3 rpow2.
 
@@ -36,11 +41,11 @@ def DescaleAAMod(src: vs.VideoNode,
     :type thr:          int
     :param kernel:      Downscaling kernel, defaults to 'bilinear'
     :type kernel:       str
-    :param b:           Downscaling parameter used in fvf.Resize, defaults to 1/3
+    :param b:           Downscaling parameter used in fvf.Resize, defaults to 0
     :type b:            var
-    :param c:           Downscaling parameter used in fvf.Resize, defaults to 1/3
+    :param c:           Downscaling parameter used in fvf.Resize, defaults to 1/2
     :type c:            var
-    :param taps:        Downscaling parameter used in fvf.Resize, defaults to 3
+    :param taps:        Downscaling parameter used in fvf.Resize, defaults to 4
     :type taps:         int
     :param expand:      Number of times to expand the difference mask, defaults to 3
     :type expand:       int
@@ -53,7 +58,7 @@ def DescaleAAMod(src: vs.VideoNode,
     :rtype:             VideoNode
     """
     import fvsfunc as fvf
-    import nnedi3_resample as nnrs
+    from nnedi3_resample import nnedi3_resample
 
     if kernel.lower().startswith('de'):
         kernel = kernel[2:]
@@ -77,7 +82,8 @@ def DescaleAAMod(src: vs.VideoNode,
     # Fix lineart
     src_y = core.std.ShufflePlanes(src, planes=0, colorfamily=vs.GRAY)
     deb = fvf.Resize(src_y, w, h, kernel=kernel, a1=b, a2=c, taps=taps, invks=True)
-    sharp = nnrs.nnedi3_resample(deb, ow, oh, invks=True,invkstaps=2,kernel="bicubic", a1=0.70, a2=0, nns=4, qual=2, pscrn=4)
+    sharp = nnedi3_resample(deb, ow, oh, invks=True,invkstaps=2, kernel="bicubic",
+                            a1=0.70, a2=0, nns=4, qual=2, pscrn=4)
     edgemask = core.std.Prewitt(sharp, planes=0)
 
     if kernel == "bicubic" and c >= 0.7:
@@ -97,7 +103,8 @@ def DescaleAAMod(src: vs.VideoNode,
     out_y = core.std.MaskedMerge(src, sharp, mask, planes=0)
 
 	#scale chroma
-    new_uv = nnrs.nnedi3_resample(src, ow, oh, invks=True,invkstaps=2,kernel="gauss", a1=30, nns=4, qual=2, pscrn=4 ,chromak_down="gauss", chromak_down_invks=True, chromak_down_invkstaps=2, chromak_down_taps=1, chromak_down_a1=16)
+    new_uv = nnedi3_resample(src, ow, oh, invks=True, invkstaps=2, kernel="gauss", a1=30, nns=4, qual=2, pscrn=4 ,chromak_down="gauss",
+                             chromak_down_invks=True, chromak_down_invkstaps=2, chromak_down_taps=1, chromak_down_a1=16)
     edgemask = core.std.Prewitt(new_uv, planes=0)
     edgemask_uv = core.std.Invert(edgemask, planes=[0])
 
@@ -117,5 +124,4 @@ def DescaleAAMod(src: vs.VideoNode,
 
     if showmask:
         out = mask
-
     return out
